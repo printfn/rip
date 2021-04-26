83686411e487489c7858f2bea01ed331ccf33af3
printfn <printfn@users.noreply.github.com>
2021-04-27 08:43:59 +1200

    Improve debug output

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ struct Voxels {
     int width = 0;
     int height = 0;
     std::vector<int> voxels;
+
+    int maxX() const {
+        return voxels.size() / width / height;
+    }
+    int maxY() const {
+        return height;
+    }
+    int maxZ() const {
+        return width;
+    }
 };
 
 int get(const Voxels &v, int x, int y, int z) {
@@ -15,8 +25,19 @@ void set(Voxels &v, int x, int y, int z, int value) {
     v.voxels[x * v.width * v.height + y * v.width + z] = value;
 }
 
-void print(const Voxels &v) {
-    printf("Dimensions: %lux%ix%i\n", v.voxels.size() / v.width / v.height, v.height, v.width);
+void print(const Voxels &v, bool detailed = false) {
+    printf("Dimensions: %ix%ix%i\n", v.maxX(), v.maxY(), v.maxZ());
+    if (detailed) {
+        for (int x = 0; x < v.maxX(); ++x) {
+            for (int y = 0; y < v.maxX(); ++y) {
+                for (int z = 0; z < v.maxX(); ++z) {
+                    printf("%i", get(v, x, y, z));
+                }
+                printf("\n");
+            }
+            printf("\n\n");
+        }
+    }
 }
 
 Voxels makeCube(int length) {
