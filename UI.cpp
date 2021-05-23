42a5bf6b1652dc1d805e7389e9036382d7e74ea3
printfn <printfn@users.noreply.github.com>
2021-05-23 13:55:46 +1200

    Rotating triangle

--- UI.cpp
+++ UI.cpp
@@ -1,5 +1,36 @@
+#include "linmath.h"
+
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+static const struct {
+    float x, y;
+    float r, g, b;
+} vertices[3] = {
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+ 
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     (void)scancode;
@@ -10,7 +41,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 void error_callback(int error, const char *description) {
-    std::cerr << "Error code " << error << ": " << description << std::endl;
+    std::cerr << "Error: " << description << " (error code " << error << ")" << std::endl;
 }
 
 int initGlfw() {
@@ -22,23 +53,72 @@ int initGlfw() {
         return -1;
 
     // Create a windowed mode window and its OpenGL context
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     GLFWwindow *window = glfwCreateWindow(
         640, 480, "Recursive Interlocking Puzzles", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
+        std::cerr << "GLFW Window creation failed" << std::endl;
         return -1;
     }
 
     // Make the window's context current
     glfwMakeContextCurrent(window);
     glClearColor(0.4f, 0.3f, 0.4f, 0.0f);
-
+    glfwSwapInterval(1);
     glfwSetKeyCallback(window, key_callback);
+
+    // Setup
+    GLuint vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+ 
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+ 
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+ 
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+ 
+    GLint mvp_location = glGetUniformLocation(program, "MVP");
+    GLint vpos_location = glGetAttribLocation(program, "vPos");
+    GLint vcol_location = glGetAttribLocation(program, "vCol");
+ 
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(vertices[0]), nullptr);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(vertices[0]), (void *)(sizeof(float) * 2));
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
         // Render here
+        mat4x4 m, p, mvp;
+
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        float ratio = width / (float)height;
+
+        glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
+ 
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+ 
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -47,6 +127,8 @@ int initGlfw() {
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
+    
     glfwTerminate();
     return 1;
 }
