03b3ba736e1e45f674e91327a157e62d6074aff1
printfn <printfn@users.noreply.github.com>
2021-05-23 13:41:17 +1200

    Set glfw error handler

--- UI.cpp
+++ UI.cpp
@@ -1,4 +1,5 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     (void)scancode;
@@ -8,12 +9,19 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
+void error_callback(int error, const char *description) {
+    std::cerr << "Error code " << error << ": " << description << std::endl;
+}
+
 int initGlfw() {
-    /* Initialize the library */
+    // Set error callback, this can happen before GLFW initialisation
+    glfwSetErrorCallback(error_callback);
+
+    // Initialize the library
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    // Create a windowed mode window and its OpenGL context
     GLFWwindow *window = glfwCreateWindow(
         640, 480, "Recursive Interlocking Puzzles", nullptr, nullptr);
     if (!window) {
@@ -21,21 +29,21 @@ int initGlfw() {
         return -1;
     }
 
-    /* Make the window's context current */
+    // Make the window's context current
     glfwMakeContextCurrent(window);
     glClearColor(0.4f, 0.3f, 0.4f, 0.0f);
 
     glfwSetKeyCallback(window, key_callback);
 
-    /* Loop until the user closes the window */
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
+        // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events
         glfwPollEvents();
     }
 
