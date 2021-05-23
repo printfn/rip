ad404445e7651a50c46eb550bf78fbafb8a80250
printfn <printfn@users.noreply.github.com>
2021-05-23 13:59:11 +1200

    Clean up string literals

--- UI.cpp
+++ UI.cpp
@@ -12,25 +12,27 @@ static const struct {
     {   0.f,  0.6f, 0.f, 0.f, 1.f }
 };
 
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
+static const char* vertex_shader_text = R"(
+#version 110
+uniform mat4 MVP;
+attribute vec3 vCol;
+attribute vec2 vPos;
+varying vec3 color;
+
+void main() {
+    gl_Position = MVP * vec4(vPos, 0.0, 1.0);
+    color = vCol;
+}
+)";
  
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
+static const char* fragment_shader_text = R"(
+#version 110
+varying vec3 color;
+
+void main() {
+    gl_FragColor = vec4(color, 1.0);
+}
+)";
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     (void)scancode;
@@ -53,8 +55,6 @@ int initGlfw() {
         return -1;
 
     // Create a windowed mode window and its OpenGL context
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     GLFWwindow *window = glfwCreateWindow(
         640, 480, "Recursive Interlocking Puzzles", nullptr, nullptr);
     if (!window) {
@@ -128,7 +128,7 @@ int initGlfw() {
     }
 
     glfwDestroyWindow(window);
-    
+
     glfwTerminate();
     return 1;
 }
